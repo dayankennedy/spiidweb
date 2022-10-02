4621e69ab19e878ee7c5fe5c14789078854d1547
dayankennedyspiid <dayankennedyspiid@gmail.com>
2022-10-02 22:31:57 +0300

    templaten file added

--- spiidapp/views.py
+++ spiidapp/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 
 # Create your views here.
 
-def base(request):
-    return render(request, 'base.html')
+def index(request):
+    return render(request, 'index.html')
